d16e386247cbb66ea74c7561ce7fb1a58cbb6dbd
k-kahora <mango22mjk@gmail.com>
2023-09-22 00:09:13 -0400

    final commit

--- flake.nix
+++ flake.nix
@@ -12,13 +12,14 @@
 		};
 
 	};
-	outputs = {self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+	outputs = {self, nixpkgs, flake-utils, ... }: 
 		let
+			system = "x86_64-linux";
 			pkgs = import nixpkgs {
 				inherit system;
 			};
 		in rec {
-			packages = {
+			packages.${system} = {
 				default = pkgs.poetry2nix.mkPoetryApplication { projectDir = self; };
 			};
 			apps = rec {
@@ -34,6 +35,5 @@
 					poetry
 				];
 			};
-		}
-	);
+		};
 }
